2229fbf5ae4e85dbb0b6cd8f3652ce175ae3b757
Jon <55158803+Jlopez0307@users.noreply.github.com>
2023-06-01 20:43:25 -0400

    finished graphs exercise

---
+++ graph.js
@@ -0,0 +1,91 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  };
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for(let node of vertexArray){
+      this.addVertex(node);
+    };
+  };
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  };
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  };
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node.adjacent.has(vertex)) {
+        node.adjacent.delete(vertex);
+      }
+    }
+    this.nodes.delete(vertex);
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+    const stack = [start];
+    const nodeArr = [];
+    const seen = new Set;
+    let currentVertex;
+
+    seen.add(start);
+
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          stack.push(neighbor);
+        }
+      });
+    }
+    return nodeArr;
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+    let visitQueue = [start];
+    let nodeArr = [];
+    let seen = new Set(start);
+
+    while(visitQueue.length){
+      let currNode = visitQueue.shift();
+      nodeArr.push(currNode.value);
+
+      currNode.adjacent.forEach(neighbor => {
+        if (!seen.has(neighbor)) {
+          seen.add(neighbor);
+          visitQueue.push(neighbor);
+        }
+      })
+    }
+    return nodeArr;
+  }
+}
+
+module.exports = {Graph, Node}
